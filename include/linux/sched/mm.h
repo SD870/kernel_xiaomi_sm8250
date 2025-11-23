b54e71ca60c13d03d0be6409c88a3553cfb4948d
Xie Yuanbin <qq570070308@gmail.com>
2025-11-23 20:18:27 +0800

    sched/core: Make finish_task_switch() and its subfunctions always inline
    finish_task_switch() is a hot code path in context switching.
    When spectre_v2_user is enabled, kernel is likely to perform branch
    prediction hardening inside switch_mm_irqs_off(). finish_task_switch()
    is right after switch_mm_irqs_off(), so the performance here is
    greatly affected by function calls and branch jumps.
    
    Make finish_task_switch() always inline to optimize performance.
    
    After finish_task_switch() is changed as always inline, the number of
    calling points of subfunctions increases. According to the compiler
    optimization strategy, subfunctions that were originally inline may no
    longer be inline.
    
    Also make the subfunctions of finish_task_stwitch() always inline to
    prevent performance degradation.
    
    There is a improvement in the performace of finish_task_switch(). When
    spectre v2 is enabled, the improvement is significant.
    
    The following are testing results from intel i5-8300h@4Ghz (x86):
    Time spent on calling finish_task_switch(), the unit is tsc from x86:
     | test scenario             | old   | new   | delta          |
     | gcc 15.2                  | 13.94 | 12.40 | 1.54  (-11.1%) |
     | gcc 15.2 + spectre_v2     | 24.78 | 13.70 | 11.08 (-44.7%) |
     | clang 21.1.4              | 13.90 | 12.71 | 1.19  (- 8.6%) |
     | clang 21.1.4 + spectre_v2 | 29.01 | 18.91 | 10.1  (-34.8%) |
    
    There is a minor improvement in the size of .text section in vmlinux,
    the unit is bytes:
     | test scenario             | old      | new      | delta |
     | gcc 15.2                  | 16208096 | 16208736 | 640   |
     | clang 21.1.4              | 17943328 | 17944224 | 896   |
    
    No size changes were found on bzImage.
    
    Signed-off-by: Xie Yuanbin <qq570070308@gmail.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Rik van Riel <riel@surriel.com>
    Cc: Segher Boessenkool <segher@kernel.crashing.org>
    Cc: David Hildenbrand (Red Hat) <david@kernel.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: H. Peter Anvin (Intel) <hpa@zytor.com>
    Cc: Arnd Bergmann <arnd@arndb.de>
    ---
    Additional information:
    1. The default spectre_v2 mitigation strategy on x86 is conditional, it
    can be set to always by appending "spectre_v2_user=on" to cmdline.
    However, on arm64/arm32, spectre v2 mitigation is enabled by default, and
    it should be disabled by adding "nospectre_v2" to the cmdline.
    
    2. The testing kernel source is commit 9c0826a5d9aa4d52206d ("Add
    linux-next specific files for 20251107") from linux-next branch.
    
    3. When testing for performace, I use the default x86_64_defconfig,
    and setting:
    CONFIG_HZ=100
    CONFIG_DEBUG_ENTRY=n
    CONFIG_X86_DEBUG_FPU=n
    CONFIG_EXPERT=y
    CONFIG_MODIFY_LDT_SYSCALL=n
    CONFIG_CGROUPS=n
    CONFIG_BLK_DEV_NVME=y
    When testing for size, I use the default x86_64_defconfig,
    and setting:
    CONFIG_SCHED_CORE=y
    CONFIG_CC_OPTIMIZE_FOR_SIZE=y
    CONFIG_NO_HZ_FULL=y
    
    4. It is recommended that using multiple processes rather than threads to
    do the test, because this will trigger switch_mm_irqs_off(), where
    spectre_v2 mitigations may be performed, during context switching.
    
    5. The test code can be found at the end of the cover letter:
    Link: https://lore.kernel.org/20251123121827.1304-1-qq570070308@gmail.com
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: kvsnr113 <kvsnrprojkt113@gmail.com>

--- include/linux/sched/mm.h
+++ include/linux/sched/mm.h
@@ -38,7 +38,7 @@ static inline void mmgrab(struct mm_struct *mm)
 
 extern void __mmdrop(struct mm_struct *mm);
 
-static inline void mmdrop(struct mm_struct *mm)
+static __always_inline void mmdrop(struct mm_struct *mm)
 {
 	/*
 	 * The implicit full barrier implied by atomic_dec_and_test() is
@@ -333,7 +333,7 @@ enum {
 #include <asm/membarrier.h>
 #endif
 
-static inline void membarrier_mm_sync_core_before_usermode(struct mm_struct *mm)
+static __always_inline void membarrier_mm_sync_core_before_usermode(struct mm_struct *mm)
 {
 	if (current->mm != mm)
 		return;
